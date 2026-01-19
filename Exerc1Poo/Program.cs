b9930bcabff1fdf98d403651f40f78918f08861e
tlima1011 <thiagolima1011@gmail.com>
2026-01-18 21:59:36 -0300

    Projeto Criado C# Exercicio 1 Poo

---
+++ Exerc1Poo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization; 
+
+namespace Exerc1Poo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo1 = new Retangulo();
+
+            Console.WriteLine("Entre a largura e altura do retângulo:");
+            retangulo1.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); 
+            retangulo1.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(retangulo1);
+            Console.WriteLine();
+
+            Retangulo retangulo2 = new Retangulo(); 
+            Console.WriteLine("Entre outra largura e altura do retângulo:");
+            retangulo2.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            retangulo2.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(retangulo2);
+        }
+    }
+}
---
+++ Exerc1Poo/Retangulo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Exerc1Poo
+{
+    internal class Retangulo
+    {
+        public double Largura; 
+        public double Altura; 
+
+        public double Area()
+        {
+            return Largura * Altura; 
+        }
+
+        public double Perimetro()
+        {
+            return 2.0 * (Largura + Altura);
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Math.Pow(Largura,2.0) + (Math.Pow(Altura, 2.0)));
+        }
+
+        public override string ToString()
+        {
+            return "AREA = " + Area().ToString("F2", CultureInfo.InvariantCulture)
+                    + "\nPERIMETRO = " + Perimetro().ToString("F2", CultureInfo.InvariantCulture)
+                    + "\nDIAGONAL = " + Diagonal().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
